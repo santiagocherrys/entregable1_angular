c6744a472e35e4ae72e999ef291cef45c6e588ae
Santiago Echevery Serna <santiagocherrys@hotmail.com>
2024-09-11 04:57:15 -0500

    Added modal, missing that show data from specific image

--- galeria/src/app/app.component.ts
+++ galeria/src/app/app.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'galeria';
   public categoria: string = "dado"
   // Variable to store the selected option
   public seleccion: string = 'todo';
-
+  
   imagenes = [
     {id: 1, imagen: 'dice1', titulo: 'titulo1' , descripcion: 'imagen 1' , category: 'dado'},
     {id: 2, imagen: 'dice2', titulo: 'titulo2' , descripcion: 'imagen 2' , category: 'dado2'},
@@ -32,5 +33,23 @@ export class AppComponent {
     // You can do any logic based on the selected option here
     this.seleccion = value;
     console.log('Selected Option:', this.seleccion);
+  }
+
+  //Modal
+  private modalElement: HTMLElement | null = null;
+  private modalInstance: any; // Use `any` to avoid type issues
+
+  ngAfterViewInit(): void {
+    // Initialize the modal instance after the view has been initialized
+    this.modalElement = document.getElementById('exampleModal');
+    if (this.modalElement) {
+      this.modalInstance = new (window as any).bootstrap.Modal(this.modalElement);
+    }
+  }
+
+  openModal(): void {
+    if (this.modalInstance) {
+      this.modalInstance.show();
+    }
   }
 }
