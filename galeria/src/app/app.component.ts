73e33493f96316a227eaa94f676e60c2bc721829
Santiago Echevery Serna <santiagocherrys@hotmail.com>
2024-09-11 04:33:40 -0500

    show image with ngFor and added selector to show by category

--- galeria/src/app/app.component.ts
+++ galeria/src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'galeria';
+  public categoria: string = "dado"
+  // Variable to store the selected option
+  public seleccion: string = 'todo';
+
+  imagenes = [
+    {id: 1, imagen: 'dice1', titulo: 'titulo1' , descripcion: 'imagen 1' , category: 'dado'},
+    {id: 2, imagen: 'dice2', titulo: 'titulo2' , descripcion: 'imagen 2' , category: 'dado2'},
+    {id: 3, imagen: 'dice3', titulo: 'titulo3' , descripcion: 'imagen 3' , category: 'dado'},
+    {id: 4, imagen: 'dice4', titulo: 'titulo4' , descripcion: 'imagen 4' , category: 'dado2'},
+    {id: 5, imagen: 'dice5', titulo: 'titulo5' , descripcion: 'imagen 5' , category: 'dado'},
+    {id: 6, imagen: 'dice6', titulo: 'titulo6' , descripcion: 'imagen 6' , category: 'dado2'}
+  ]
+
+  // Mostrar ID
+  showId(id: number): void {
+    console.log('Image ID:', id);
+  }
+
+  
+
+  // Method to handle changes in the dropdown selection
+  onOptionChange(value:string): void {
+    // You can do any logic based on the selected option here
+    this.seleccion = value;
+    console.log('Selected Option:', this.seleccion);
+  }
 }
